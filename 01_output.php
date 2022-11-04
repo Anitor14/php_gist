8afabad06e3c7dbb8ccea10e1b84e013450381eb
Anitor <anitorabraham@gmail.com>
2022-11-04 01:38:57 +0100

    php reference cheat

---
+++ 01_output.php
@@ -0,0 +1,28 @@
+<?php
+
+// echo "Hello 123";
+
+// print 133;
+
+// print_r([1,2,3]);
+
+// var_dump(true);
+
+// var_export('hello');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><?php echo "post"?>
+        <h1>
+</body>
+
+</html>
---
+++ 07_array_functions.php
@@ -0,0 +1,12 @@
+<?php
+
+$fruits = ['apple', 'orange', 'pear'];
+
+// GEt length
+echo count($fruits);
+
+//search array
+
+var_dump(in_array('apple', $fruits));
+
+// add to array
